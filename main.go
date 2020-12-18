bdb6b378cf2289f554424b74353076a14ebf1fbc
Michael Jarvis <michael@jarvis.com>
2020-12-18 15:37:15 -0600

    move to one file

--- main.go
+++ main.go
@@ -1,15 +1,13 @@
 package main
 
 import (
-	// _ "github.com/go-sql-driver/mysql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/mysql"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 	"os"
 )
-
-
 
 func main() {
 	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
@@ -39,4 +37,20 @@ func main() {
 	}
 
 	log.Info().Msg("Shutting down")
+}
+
+func getDatabaseString() string {
+	sqlUsername := os.Getenv("MYSQL_USER")
+	sqlPassword := os.Getenv("MYSQL_PASS")
+	sqlHost := "127.0.0.1"
+	sqlPort := 33060
+
+	log.Debug().Msgf("The value of sqlUsername is '%s'", sqlUsername)
+	log.Debug().Msgf("The value of sqlPassword is '%s'", sqlPassword)
+	log.Debug().Msgf("The value of sqlHost is '%s'", sqlHost)
+	log.Debug().Msgf("The value of sqlPort is (%T) '%d'", sqlPort, sqlPort)
+	stmt := fmt.Sprintf("%s:%s@tcp(%s:%d)/?charset=utf8&parseTime=True&loc=Local", sqlUsername, sqlPassword, sqlHost, sqlPort)
+	log.Debug().Msgf("Statement: '%s'", stmt)
+
+	return stmt
 }
